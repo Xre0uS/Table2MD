6eda97c1e7d8152b31aaf11ae66d05a629d95cee
Xre0uS <hello@xre0us.io>
2023-08-11 04:38:31 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# HTML table to markdown+
-Converts HTML tables to Markdown, preserves hyperlinks, inline code blocks and more,
+# HTML Table to Markdown+
+
+Functions:
+- Extracts and converts multiple tables (even whole page source)
+- Converts links in table
+- Converts code tags in table
+- Escape pipe characters
+- Use first row as header or insert an empty first row
+- Bold headers
+- Bold first column
+- First column as code
+
+## Example
+
+HTML table:
+<body>
+    <table>
+        <tr>
+            <th>Fruit</th>
+            <th>Color</th>
+            <th>Taste</th>
+        </tr>
+        <tr>
+            <td>Apple<a href="https://apple.com"> apple.com<a></td>
+            <td>Red, Green, Yellow</td>
+            <td>Sweet</td>
+        </tr>
+        <tr>
+            <td>Banana</td>
+            <td>Yellow<code>#FFE135</code></td>
+            <td>Sweet</td>
+        </tr>
+        <tr>
+            <td>Orange</td>
+            <td>Orange</td>
+            <td>Citrusy | sour</td>
+        </tr>
+    </table>
+</body>
+
+```
+<table>
+    <tr>
+        <th>Fruit</th>
+        <th>Color</th>
+        <th>Taste</th>
+    </tr>
+    <tr>
+        <td>Apple<a href="https://apple.com"> apple.com<a></td>
+        <td>Red, Green, Yellow</td>
+        <td>Sweet</td>
+    </tr>
+    <tr>
+        <td>Banana</td>
+        <td>Yellow<code>#FFE135</code></td>
+        <td>Sweet</td>
+    </tr>
+    <tr>
+        <td>Orange<code>#FFA500</code></td>
+        <td>Orange</td>
+        <td>Citrusy | sour</td>
+    </tr>
+</table>
+```
+
+Converts to:
+| Fruit | Color | Taste |
+| --- | --- | --- |
+| Apple[ apple.com](https://apple.coom) | Red, Green, Yellow | Sweet |
+| Banana | Yellow`#FFE135` | Sweet |
+| Orange | Orange | Citrusy \| sour |
+
+I'm surprised to find no table converters around that converts links or code tags, in fact, the only instances that I could find that do it properly is [this Debian package](https://manpages.debian.org/testing/python3-html2text/html2markdown.py3.1.en.html) and [turndown](https://github.com/mixmark-io/turndown) with [turndown-plugin-gfm](https://github.com/mixmark-io/turndown-plugin-gfm). But what if I just want to quickly copy/paste a table? Here is my take on it.
--- converter.js
+++ converter.js
@@ -1,23 +1,26 @@
-var input, output, copyBtn, convertBtn, messageBox, checkBoxes, processor, optionsArray = [];
+const newLine = "\n";
+const markdownCellSeparator = " | "
+var inputBox, outputBox, copyBtn, convertBtn, messageBox, checkBoxes, buffer, tableColumnCount, optionsArray, errorArray;
 
 function setup() {
-    input = document.getElementById("html-input");
-    output = document.getElementById("markdown-output");
+    inputBox = document.getElementById("html-input");
+    outputBox = document.getElementById("markdown-output");
     copyBtn = document.getElementById("btn-copy");
     convertBtn = document.getElementById("btn-convert");
     messageBox = document.getElementById("message-box");
-    processor = document.getElementById("processor");
+    buffer = document.getElementById("buffer");
     checkBoxes = document.querySelectorAll('input[type="checkbox"]');
     copyBtn.disabled = true;
-    output.value = "";
+    outputBox.value = "";
 
     convertBtn.addEventListener("click", function () {
         getCheckboxValues();
-        convertTable();
+        convertTables();
     });
 }
 
 function getCheckboxValues() {
+    optionsArray = [];
     checkBoxes.forEach(function (checkbox) {
         optionsArray.push({
             value: checkbox.value, checked: checkbox.checked
@@ -25,12 +28,17 @@ function getCheckboxValues() {
     });
 }
 
-function convertTable() {
-    processor.innerHTML = input.value.replace(/\s+/g, ' ');
-    var tables = processor.getElementsByTagName("table");
+function convertTables() {
+    errorArray = [];
+    var convertedOutput = "";
+    buffer.innerHTML = inputBox.value.replace(/\s+/g, ' ');
+    var tables = buffer.getElementsByTagName("table");
 
-    if (tables) {
-        displayMessage("Tables found")
+    if (tables.length > 0) {
+        for (var i = 0; i < tables.length; i++) {
+            convertedOutput += convertTableElements(i, tables[i]) + newLine + newLine;
+            displayOutput(tables.length, convertedOutput.trim())
+        }
     }
     else {
         displayMessage("No tables found")
@@ -38,16 +46,139 @@ function convertTable() {
 }
 
 function getChecked(valueName) {
-    const option = optionsArray.find(option => option.value === valueName);
+    const option = optionsArray.find(option => option.value == valueName);
     return option.checked;
 }
 
-function displayOutput(outputData) {
-    output.value = outputData;
-    copyBtn.disabled = false;
-    copyBtn.addEventListener("click", function () {
-        writeToClipboard(outputData);
+function convertTableElements(tableNumber, table) {
+    var convertedRows = [];
+    var rows = table.getElementsByTagName("tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var convertedRow = convertRowElements(i, rows[i]);
+
+        if (convertedRow == "") {
+            errorArray[errorArray.length - 1].table = tableNumber + 1;
+            return "";
+        }
+        convertedRows.push(convertedRow)
+    }
+    return convertedRows.join(newLine)
+}
+
+function convertRowElements(rowNumber, rowContent) {
+    var convertedRow = [""];
+    var rowElements = rowContent.children;
+
+    for (var i = 0; i < rowElements.length; i++) {
+        var convertedElement = processOptions(rowElements[i], rowNumber, i);
+
+        convertedRow.push(convertedElement);
+    }
+    convertedRow.push("");
+
+    if (rowNumber == 0) {
+        tableColumnCount = rowElements.length;
+        var [headerRow, dividerRow ] = createDividerRow(rowElements.length);
+        if (document.getElementById("header-first-row").checked) {
+            return convertedRow.join(markdownCellSeparator).trim() + newLine + dividerRow;
+        }
+        else {
+            return headerRow + newLine + dividerRow + newLine + convertedRow.join(markdownCellSeparator).trim()
+        }
+    }
+    else if (tableColumnCount != rowElements.length) {
+        errorArray.push({ table: 0, row: rowNumber + 1 });
+        return "";
+    }
+
+    return convertedRow.join(markdownCellSeparator).trim();
+}
+
+function processOptions(elementData, rowNumber, columnIndex) {
+    optionsArray.forEach((option) => {
+        if (option.checked && optionsFunctions.hasOwnProperty(option.value)) {
+            elementData = optionsFunctions[option.value](elementData, rowNumber, columnIndex);
+        }
     });
+
+    return elementData.textContent;
+}
+
+var optionsFunctions = {
+    convertLink: (elementData) => {
+        elementData.innerHTML = elementData.innerHTML.replace(/<a href="(.*?)".*?>(.*?)<\/a>/g, '[$2]($1)');
+        return elementData;
+    },
+    convertCode: (elementData) => {
+        elementData.innerHTML = elementData.innerHTML.replace(/<code>(.*?)<\/code>/g, '`$1`')
+        return elementData;
+    },
+    escPipe: (elementData) => {
+        elementData.innerHTML = elementData.innerHTML.replace(/\|/g, '\\|')
+        return elementData;
+    },
+    boldHeader: (elementData) => {
+        elementData.innerHTML = elementData.outerHTML.replace(/<th>(.*?)<\/th>/g, '**$1**');
+        return elementData;
+    },
+    boldCol: (elementData, rowNumber, columnIndex) => {
+        if (rowNumber > 0 && columnIndex == 0) {
+            elementData.innerHTML = "**" + elementData.innerHTML + "**";
+            return elementData;
+        }
+        return elementData;
+    },
+    codeCol: (elementData, rowNumber, columnIndex) => {
+        if (rowNumber > 0 && columnIndex == 0) {
+            elementData.innerHTML = elementData.innerHTML.replace(/`/g, '');
+            elementData.innerHTML = "`" + elementData.innerHTML + "`";
+            return elementData;
+        }
+        return elementData;
+    }
+}
+
+function createDividerRow(cellCount) {
+    var headerRow = [""];
+    var dividerRow = [newLine];
+    for (i = 0; i < cellCount; i++) {
+        headerRow.push("");
+        dividerRow.push("---")
+    }
+    headerRow.push("");
+    dividerRow.push("");
+    return [headerRow.join(markdownCellSeparator).trim(), dividerRow.join(markdownCellSeparator).trim()];
+}
+
+function displayOutput(tableCount, outputData) {
+    if (errorArray.length == 0) {
+        displayMessage(tableCount + " table(s) converted")
+    }
+    else {
+        var message = "";
+
+        errorArray.forEach(obj => {
+            Object.entries(obj).forEach(([key, value]) => {
+                message += " " + key + " " + value
+            });
+        });
+
+        displayMessage(tableCount - errorArray.length + " table(s) converted, error(s) in" + message)
+    }
+
+    if (outputData == "") {
+        copyBtn.disabled = true;
+        outputBox.value = "";
+    }
+    else {
+        outputBox.value = outputData;
+        copyBtn.disabled = false;
+
+        copyBtn.addEventListener("click", function () {
+            writeToClipboard(outputData);
+        });
+    }
 }
 
 function displayCheckboxValues() {
@@ -59,12 +190,12 @@ function displayCheckboxValues() {
     displayMessage(message);
 }
 
-async function writeToClipboard(text) {
+async function writeToClipboard(data) {
     try {
-        await navigator.clipboard.writeText(text);
-        displayMessage("Markdown table copied to clipboard")
+        await navigator.clipboard.writeText(data);
+        displayMessage("Copied to clipboard")
     } catch (err) {
-        displayMessage("Failed to copy markdown table to clipboard")
+        displayMessage("Failed to copy to clipboard")
     }
 }
 
